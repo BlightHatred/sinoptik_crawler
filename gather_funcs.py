be10d827ecf15be0fd4612aa7bc4b0eb02012911
BlightHatred <karamfil_tonev@abv.bg>
2022-10-10 17:49:13 +0300

    Sinoptik Crawler

---
+++ gather_funcs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#those are the functions used to gather specific time and temperature data from sinoptik.bg
+def dategather(soup):
+    ByHourDate = soup.find('span', 'wfByHourDate')
+
+    date = ByHourDate.text
+
+
+    return date
+    
+def hourgather(soup):
+    ByHourHour = soup.find_all('span', 'wfByHourHour')
+    
+    hours = []
+
+    index = 0
+    while index < 12:
+        hour = ByHourHour.pop(0)
+        hours.append(hour.text)
+        index += 1
+
+    return hours
+
+def tempgather(soup):
+    ByHourTemp = soup.find_all('span', 'wfByHourTemp')
+    
+    temps = []
+
+    for item in ByHourTemp:
+        if ByHourTemp.index(item) <= 24 and ByHourTemp.index(item) % 2 == 1:
+            temps.append(item.text)
+    return temps
---
+++ sinoptik_crawler.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup as bs
+import requests
+from gather_funcs import dategather, hourgather, tempgather
+import csv
+
+#requesting page from sinoptik.bg
+link = "https://www.sinoptik.bg/plovdiv-bulgaria-100728193/hourly"
+page = requests.get(link)
+tree = page.text
+
+soup = bs(tree, 'html.parser')
+
+#calling data gathering functions
+hours = hourgather(soup)
+
+temps = tempgather(soup)
+
+date = dategather(soup)
+
+#writing data down in a CSV file and printing it in terminal
+header = [f'Дата : {date}', 'Час', 'Температура']
+with open('/home/blight/data.csv', 'w', newline='', encoding='utf-8') as datafile:
+    datawriter = csv.writer(datafile, delimiter=',', 
+        quoting=csv.QUOTE_MINIMAL)
+    datawriter.writerow(header)
+    for i in range(12):
+        data = [None, hours[i], temps[i]]
+        datawriter.writerow(data)
+        print(data)
+  
+
